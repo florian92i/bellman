f3c7d92afbf5ecd974412ec5bcb21208380d7915
Florian COMPIEGNE <florian.compiegne@eemi.com>
2020-05-06 11:33:36 +0200

    b

--- bellmanford.py
+++ bellmanford.py
@@ -36,8 +36,8 @@ def plus_court(graphe, etape, fin, visites, dist, pere, depart):
     """
     # si on arrive à la fin, on affiche la distance et les peres
 
-    print('etape -->', etape)
-    print('fin -->', fin)
+   # print('etape -->', etape)
+   # print('fin -->', fin)
     if etape == fin:
         return dist[fin], affiche_peres(pere, depart, fin, [])
     # si c'est la première visite, c'est que l'étape actuelle est le départ : on met dist[etape] à 0
@@ -78,7 +78,16 @@ if __name__ == "__main__":
           }
     l4, c4 = dij_rec(g4, 'a', 'h')
     print('Plus court chemin ex4 : ', c4, ' de longueur :', l4)
-
+    arrayKey = []
+    for clee in g4:
+        arrayKey.append(clee)
+    print(arrayKey)
+    for clee in g4.values():
+        print('clee', clee)  # clee
+        print('valeur', clee)  # value
+        for cle, valeur in clee.items():
+            print('valeur', valeur)
+            print('cle', cle)
 
 G = nx.DiGraph()
 
@@ -88,7 +97,7 @@ G.add_edges_from([('B', 'E'), ('C', 'E'), ('E', 'G')], weight=4)
 G.add_edges_from([('A', 'D'), ('D', 'F'), ('F', 'H')], weight=5)
 G.add_edges_from([('C', 'G')], weight=6)
 G.add_edges_from([('A', 'E')], weight=7)
-
+G.add_edges_from([('C', 'G')], weight=9)
 
 edge_labels = dict([((u, v,), d['weight'])
                     for u, v, d in G.edges(data=True)])
@@ -97,12 +106,10 @@ edge_colors = ['black' if not edge in red_edges else 'red' for edge in G.edges()
 
 pos = nx.spring_layout(G)
 nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
-nx.draw_networkx(G, pos, node_size=1500, edge_color=edge_colors, edge_cmap=plt.cm.Reds)
-pylab.show()
+nx.draw_networkx(G, pos, node_size=2500, edge_color=edge_colors, edge_cmap=plt.cm.Reds)
+# pylab.show()
 """
 Réponse :
 Plus court chemin ex3 :  ['e', 'c', 'f', 's']  de longueur : 10
 Plus court chemin ex4 :  ['a', 'b', 'e', 'g', 'h']  de longueur : 13
 """
-
-
